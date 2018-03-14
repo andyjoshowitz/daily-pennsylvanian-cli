9d43ec50ccdda2a6abe4bc722099add75ede687c
Andrew Joshowitz <ajoshow@sas.upenn.edu>
2018-03-14 21:50:02 +0000

    fixed requirements again

--- lib/daily_pennsylvanian.rb
+++ lib/daily_pennsylvanian.rb
@@ -1,12 +1,9 @@
-
-
-
 require 'open-uri'
 require 'pry'
 require 'nokogiri'
 
-require_relative "daily_pennsylvanian/article.rb"
-require_relative "daily_pennsylvanian/section.rb"
-require_relative "daily_pennsylvanian/sub_section.rb"
-require_relative "daily_pennsylvanian/cli.rb"
-require_relative "daily_pennsylvanian/version.rb"
+require_relative "./daily_pennsylvanian/article.rb"
+require_relative "./daily_pennsylvanian/section.rb"
+require_relative "./daily_pennsylvanian/sub_section.rb"
+require_relative "./daily_pennsylvanian/cli.rb"
+require_relative "./daily_pennsylvanian/version.rb"
--- lib/daily_pennsylvanian/cli.rb
+++ lib/daily_pennsylvanian/cli.rb
@@ -13,8 +13,20 @@ class CLI
 
   def menu
     # list menu options: re-list sections, exit, select section
+    puts "1) To select a Section of the newspaper, enter the Section number."
+    puts "2) To see a list of the paper's Sections, enter 'list'."
+    puts "3) To exit the program, enter 'exit'."
     # get input
+    input = gets.strip
     # call methods (list_again, exit, open_section) depending on what the user inputs
+    case input
+    when "exit"
+      exit_program
+    when "list"
+      list_sections
+    else
+      open_section
+    end
   end
 
   def list_again
