b100c896ce3b96013c4ab49b138e17896c236d2e
Andrew Joshowitz <ajoshow@sas.upenn.edu>
2018-03-14 18:34:13 +0000

    wrote introdcutory methods: call, welcome, menu...

---
+++ lib/daily_pennsylvanian/cli.rb
@@ -0,0 +1,28 @@
+class DailyPennsylvanian::CLI
+  def call
+    welcome
+    menu
+  end
+
+  def welcome
+    puts "Welcome to the Daily Pennsylvanian!"
+    puts ""
+    puts "Which section piques your interest?"
+    # scrape the website for the website names
+  end
+
+  def menu
+    # list menu options: re-list sections, exit, select section
+    # get input
+    # call methods (list_again, exit, open_section) depending on what the user inputs
+  end
+
+  def list_again
+  end
+
+  def exit
+  end
+
+  def open_section 
+  end
+end
