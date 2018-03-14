2c66a4e3db4efe13e083956a7505cd5a2c8366df
Andrew Joshowitz <ajoshow@sas.upenn.edu>
2018-03-14 22:18:13 +0000

    edited welcome and menu methods

--- lib/daily_pennsylvanian/cli.rb
+++ lib/daily_pennsylvanian/cli.rb
@@ -13,26 +13,34 @@ class CLI
 
   def menu
     # list menu options: re-list sections, exit, select section
+    puts ""
+    puts "Menu"
     puts "1) To select a Section of the newspaper, enter the Section number."
-    puts "2) To see a list of the paper's Sections, enter 'list'."
+    puts "2) To see a list of the paper's Sections again, enter 'list'."
     puts "3) To exit the program, enter 'exit'."
     # get input
-    input = gets.strip
+    input = ""
     # call methods (list_again, exit, open_section) depending on what the user inputs
-    case input
-    when "exit"
-      exit_program
-    when "list"
-      list_sections
-    else
-      open_section
+    while input != "exit"
+      input = gets.strip
+      case input
+      when "exit"
+        exit_program
+        break
+      when "list"
+        list_sections
+      else
+        open_section
+      end
     end
   end
 
-  def list_again
+  def list_sections
+
   end
 
-  def exit
+  def exit_program
+    puts "Thanks for visiting the DP. See you later!"
   end
 
   def open_section
