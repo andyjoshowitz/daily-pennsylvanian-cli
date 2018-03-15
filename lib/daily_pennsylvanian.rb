be37e17e206df5de913b118a161940c4ba689178
Andrew Joshowitz <ajoshow@sas.upenn.edu>
2018-03-15 18:37:12 +0000

    updated requirements

--- lib/daily_pennsylvanian.rb
+++ lib/daily_pennsylvanian.rb
@@ -4,7 +4,6 @@ require 'nokogiri'
 
 require_relative "./daily_pennsylvanian/article.rb"
 require_relative "./daily_pennsylvanian/section.rb"
-require_relative "./daily_pennsylvanian/sub_section.rb"
 require_relative "./daily_pennsylvanian/cli.rb"
 require_relative "./daily_pennsylvanian/version.rb"
 require_relative "./daily_pennsylvanian/scraper.rb"
