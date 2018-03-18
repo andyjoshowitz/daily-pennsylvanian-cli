a19700973df76d28bf6c91bd78236ad3241d8175
Andrew Joshowitz <ajoshow@sas.upenn.edu>
2018-03-18 22:56:51 +0000

    updated Scraper

--- lib/daily_pennsylvanian/scraper.rb
+++ lib/daily_pennsylvanian/scraper.rb
@@ -1,28 +1,11 @@
+
 class Scraper
-  attr_accessor :section
+  attr_accessor :section, :article
 
-  def self.get_sections
-    url = "http://thedp.com"
-    doc = Nokogiri::HTML(open(url))
+  def initialize
 
-    sections = doc.search(".section-nav .header-section")
-    sections.each_with_index do |section, index|
-      indexplusone = index + 1
-      puts "#{indexplusone}) #{section.text.gsub(/\"/, "")}"
-    end
   end
-
-  def self.scrape_article_details
-    url = "http://thedp.com/section/news"
-    doc = Nokogiri::HTML(open(url))
-
-    #title = doc.search('div.col-md-8 h3.standard-link a')
-    #title.each do |title|
-      #puts title.text
-    #end
-
-class Scraper
-  attr_accessor :section
+  @@articles = []
 
   def self.get_sections
     url = "http://thedp.com"
@@ -48,16 +31,14 @@ class Scraper
       a = Article.new
       a.title = entry.search('h3.standard-link a').text
       #a.url = entry.attr("href").text.strip
-      a.timestamp = entry.search('div.timestamp').each do |timestamp|
-        puts timestamp.text
-      end
+      a.timestamp = entry.search('div.timestamp').text
       #@section.add_article(a)
-      a.url = entry.search('h3.standard-link a').each do |link|
-        puts link['href']
-      end
-      Section.add_article(a)
-    end
+      a.url = entry.search('h3.standard-link a[href]').map {|element| element["href"]}
+      puts a.title
+      @@articles << a
 
+    end
+    @@articles
   end
 
   def self.scrape_article_timestamps
@@ -87,6 +68,14 @@ class Scraper
 
     blurb = doc.search('div.row div.col-md-8').each do |blurb|
       puts blurb.text.strip
+    end
+  end
+
+  def self.print_article_details
+    @@articles.each do |article|
+      #puts Article.title
+      puts Article.timestamp
+      puts Article.url
     end
   end
 end
